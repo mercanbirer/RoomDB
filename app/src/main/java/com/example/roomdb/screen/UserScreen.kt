421715ea90b5a6cef9caec6fc8747b1909715b44
Mercan Nur Birer <mercanbirer42@gmail.com>
2023-10-11 12:36:19 +0300

    edit userscreen

--- app/src/main/java/com/example/roomdb/screen/UserScreen.kt
+++ app/src/main/java/com/example/roomdb/screen/UserScreen.kt
@@ -1,17 +1,19 @@
 package com.example.roomdb.screen
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.roomdb.R
 import com.example.roomdb.model.User
 import com.example.roomdb.vm.UserViewModel
 import kotlinx.coroutines.launch
@@ -24,28 +26,43 @@ fun UserScreen(
     val name = remember { mutableStateOf(TextFieldValue("")) }
     val surname = remember { mutableStateOf(TextFieldValue("")) }
 
-    Box {
-        Column() {
-            OutlinedTextField(
-                value = name.value,
-                onValueChange = {
-                    name.value = it
-                },
-                label = {
-                    Text(text = "Name")
-                }
-            )
-            OutlinedTextField(
-                value = surname.value,
-                onValueChange = {
-                    surname.value = it
-                },
-                label = {
-                    Text(text = "SurName")
-                }
-            )
-        }
-        Button(modifier = Modifier.padding(top = 180.dp), onClick = {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = name.value,
+            onValueChange = {
+                name.value = it
+            },
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                .fillMaxWidth(),
+            label = {
+                Text(text = stringResource(R.string.name))
+            }
+        )
+        OutlinedTextField(
+            value = surname.value,
+            onValueChange = {
+                surname.value = it
+            },
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+                .fillMaxWidth(),
+            label = {
+                Text(text = stringResource(R.string.surname))
+            }
+        )
+    }
+
+    Button(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 64.dp, end = 64.dp, top = 180.dp),
+        shape = CircleShape,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Transparent,
+            contentColor = Color.DarkGray
+        ), onClick = {
             coroutineScope.launch {
                 viewModel.insertUser(
                     User(
@@ -55,6 +72,6 @@ fun UserScreen(
                 )
             }
         }) {
-        }
+        Text(stringResource(R.string.add_button))
     }
 }
